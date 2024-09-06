721c9d63c9136685c07a581da541d87072dac341
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-09-05 20:39:55 -0500

    poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { collection, collectionData, doc, Firestore, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +9,30 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
-    
+    <ul>
+      @for (task of tasks(); track task.id) {
+        <li>{{task.title}} <button (click)="setInProgress(task)">Set in progress</button></li> 
+      } @empty {
+        no hay elementos
+      }
+    </ul>   
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'tasks';
+  tasks: Signal<any[] | undefined>;
+
+  constructor(private firestore: Firestore, private auth: Auth) {
+    this.tasks = toSignal(collectionData(collection(firestore, 'tasks')));
+  }
+
+  async setInProgress(task: any) {
+    const credentials = await signInWithEmailAndPassword(this.auth, 'user@server.tld', 'password');
+
+    await updateDoc(doc(this.firestore, `tasks/${task.id}`), {
+      status: 'in-progress',
+      owner: credentials.user.uid
+    })
+  }
 }
